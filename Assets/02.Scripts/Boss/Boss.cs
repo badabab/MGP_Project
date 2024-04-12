94c8e1673fe2d4b0beb6a29972a26c49a66d0672
badabab <ky040723@gmail.com>
2024-04-12 19:16:17 +0900

    보스 수정

--- Assets/02.Scripts/Boss/Boss.cs
+++ Assets/02.Scripts/Boss/Boss.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public enum BossType
 {
-    Jump,
+    WalkJump, Jump, Teleport, LeftRight
 }
 public class Boss : MonoBehaviour
 {
@@ -11,29 +11,68 @@ public class Boss : MonoBehaviour
     public Slider HP_Slider;
     public int HP;
     public int MaxHP = 30;
-
-    public float jumpForce = 10f;
-    public float moveSpeed = 5f;
-    public Transform playerTransform;
-    public float followDistance = 10f;
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
     private bool _isGround = true;
     public int Damage = 10;
+
+    public float jumpForce = 7f;
+    public float moveSpeed = 2f;
+    private Transform _playerTransform;
+    public float followDistance = 10f;
+
+    public float WalkJumpTime = 3;
+    private float _timer = 0;
 
     void Start()
     {
         HP = MaxHP;
         Refresh();
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _playerTransform = GameObject.Find("Player").transform;
     }
 
     void Update()
     {
-        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+        if (_isGround)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        switch(BossType)
+        {
+            case BossType.WalkJump:
+                WalkJumpType(); break;
+            case BossType.Jump:
+                JumpType(); break;
+        }
+    }
+
+
+    private void WalkJumpType()
+    {
+        _timer += Time.deltaTime;
+        float distanceToPlayer = Vector2.Distance(transform.position, _playerTransform.position);
         if (distanceToPlayer <= followDistance && _isGround)
         {
-            Vector2 direction = (playerTransform.position - transform.position).normalized;
-            rb.velocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
+            Vector2 direction = (_playerTransform.position - transform.position).normalized;
+            _rb.velocity = new Vector2(direction.x * moveSpeed, _rb.velocity.y);
+        }
+
+        if (_isGround)
+        {
+            if (_timer > WalkJumpTime)
+            {
+                Jump();
+                _timer = 0;
+            }
+        }
+    }
+    private void JumpType()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, _playerTransform.position);
+        if (distanceToPlayer <= followDistance && _isGround)
+        {
+            Vector2 direction = (_playerTransform.position - transform.position).normalized;
+            _rb.velocity = new Vector2(direction.x * moveSpeed, _rb.velocity.y);
         }
 
         if (_isGround)
@@ -42,16 +81,11 @@ public class Boss : MonoBehaviour
         }
     }
 
-    private void Refresh()
-    {
-        HP_Slider.value = HP / (float)MaxHP;
-    }
-
     private void Jump()
     {
         if (_isGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
             _isGround = false;
         }
     }
@@ -74,5 +108,9 @@ public class Boss : MonoBehaviour
         {
             _isGround = false;
         }
+    }
+    private void Refresh()
+    {
+        HP_Slider.value = HP / (float)MaxHP;
     }
 }
